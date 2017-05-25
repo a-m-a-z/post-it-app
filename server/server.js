c41fd17d36ad7397e2f39c78cb4080958b6beece
Ahmad Abdul-Aziz <ahmad.abdulaziz37@gmail.com>
2017-05-25 14:42:33 +0100

    add server file

--- server/server.js
+++ server/server.js
@@ -1,20 +1,23 @@
 const express = require('express');
 let	bodyParser = require('body-parser');
-let	firebase = require("firebase");
+
 
 let app = express();
 let port = process.env.PORT || 3000;
-let  reg = require('../server/register');
+let  register = require('../server/register');
+let  signin = require('../server/signin');
+let  group = require('../server/group');
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
-reg(app);
-
+register(app);
+signin(app);
+group(app);
 
 
 app.listen(port, () => {
-  console.log('We are live on ' + port);
+console.log('We are live on ' + port);
 });
