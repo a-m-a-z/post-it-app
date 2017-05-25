0f37bd89edb88e20904c9efbf6d004aa8756d83f
Ahmad Abdul-Aziz <ahmad.abdulaziz37@gmail.com>
2017-05-25 14:44:15 +0100

    add registration file

--- server/register.js
+++ server/register.js
@@ -1,9 +1,8 @@
 const express = require('express');
-let	bodyParser = require('body-parser');
-let	firebase = require("firebase");
+let	firebase = require('firebase');
  const app = express();
 
- firebase.initializeApp({
+firebase.initializeApp({
  	apiKey: "AIzaSyA5QqiYnZRRUPfD9TkmchFtnGs9M8eF740",
   authDomain: "postlt-54c26.firebaseapp.com",
   databaseURL: "https://postlt-54c26.firebaseio.com",
@@ -21,18 +20,24 @@ app.post('/register', (req, res) => {
         email =     req.body.email,
         password = req.body.password;
         firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then(  res.send({message: "Success: A user has been successfuly registered."})
-          )
+        .then( (user) =>{
+          firebase.database().ref("users").push({
+            username : full_name,
+            email : email,
+            password: password
+          });
+           res.send({message: "Success: A user has been successfuly registered."})
+        })
+
         .catch((err) => {
-            let errorCode = err.code;let full_name = req.body.full_name,
-                  email =     req.body.email,
-                  password = req.body.password;
+            let errorCode = err.code;
             let errorMessage = err.message;
         });
   });
 
-app.get('/register', function(req, res) {
-  	res.send('welcome to the register page!');
-  });
+//  let db = firebase.database;
+// app.get('/register', function(req, res) {
+//   	res.send(db);
+//   });
 
 };
