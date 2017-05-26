3e1f9a90da19671b70e0adc60f4bac7dc6566863
Ahmad Abdul-Aziz <ahmad.abdulaziz37@gmail.com>
2017-05-26 03:06:37 +0100

    correct the function from es5 to es6

--- server/signin.js
+++ server/signin.js
@@ -1,23 +1,21 @@
-const express = require('express');
-
+//CALL  FIREBASE  ===================
 let	firebase = require('firebase');
-const app = express();
 
+//THIS  EXPORT MODULE  ===================
+module.exports =  (app) => {
 
-module.exports = function (app) {
+// SIGN-IN ROUTES ===================
 	app.post('/signin', (req, res) => {
 
-		let  email =     req.body.email,
+		let  email = req.body.email,
 			password = req.body.password;
 		firebase.auth().signInWithEmailAndPassword(email, password)
         .then(  res.send({message: 'Success: A user has been successfuly login.'}))
       .catch((err) => {	res.send({message: 'You have error code '+err.code+' and the error message is'+err.message});
-
 });
 	});
-
 	app.get('/signin', function(req, res) {
-		res.send('welcome to the login page!');
+		res.send('SIGN-IN ROUTE ');
 	});
 
 };
