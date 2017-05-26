9740c96874be456fbb47e6d53bc28b4ceac7dfd0
Ahmad Abdul-Aziz <ahmad.abdulaziz37@gmail.com>
2017-05-26 03:04:41 +0100

    modify the variable names and make code more readable

--- server/register.js
+++ server/register.js
@@ -1,43 +1,29 @@
-const express = require('express');
-let	firebase = require('firebase');
- const app = express();
+//CALL  FIREBASE  ===================
+const	firebase = require('firebase');
 
-firebase.initializeApp({
- 	apiKey: "AIzaSyA5QqiYnZRRUPfD9TkmchFtnGs9M8eF740",
-  authDomain: "postlt-54c26.firebaseapp.com",
-  databaseURL: "https://postlt-54c26.firebaseio.com",
-  projectId: "postlt-54c26",
-  storageBucket: "postlt-54c26.appspot.com",
-  messagingSenderId: "1002590696573"
- });
+//THIS  EXPORT MODULE  ===================
+module.exports = (app) => {
 
-
-
-module.exports = function (app) {
-app.post('/register', (req, res) => {
-
-  let full_name = req.body.full_name,
-        email =     req.body.email,
-        password = req.body.password;
-        firebase.auth().createUserWithEmailAndPassword(email, password)
+	// SIGN-UP ROUTES ===================
+	app.post('/register', (req, res) => {
+		let full_name = req.body.full_name,
+			email =     req.body.email,
+			password = req.body.password;
+		firebase.auth().createUserWithEmailAndPassword(email, password)
         .then( (user) =>{
-          firebase.database().ref("users").push({
-            username : full_name,
-            email : email,
-            password: password
-          });
-           res.send({message: "Success: A user has been successfuly registered."})
-        })
+	firebase.database().ref('users').push({
+		username : full_name,
+		email : email,
+		password: password
+	});
+	res.send({message: 'Success: A user has been successfuly registered.'})
+})
+        .catch((err) => {	res.send({message: 'You have error code '+err.code+' and the error message is'+err.message});
+});
+	});
 
-        .catch((err) => {
-            let errorCode = err.code;
-            let errorMessage = err.message;
-        });
-  });
-
-//  let db = firebase.database;
-// app.get('/register', function(req, res) {
-//   	res.send(db);
-//   });
+	app.get('/signin', function(req, res) {
+		res.send('REGISTRATION ROUTE!');
+	});
 
 };
