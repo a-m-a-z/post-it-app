b0e8a059491dac935eeaad1c757d08888d5c525c
Ahmad Abdul-Aziz <ahmad.abdulaziz37@gmail.com>
2017-05-26 03:02:51 +0100

    make group roucte more readable

--- server/group.js
+++ server/group.js
@@ -1,17 +1,18 @@
+//CALL  FIREBASE  ===================
 const	firebase = require('firebase');
 
-
+//EXPORT  THIS  AS MODULE  ===================
 module.exports = function (app) {
+
+	// CREATE ROUTES ===================
 	app.post('/group', (req, res) => {
-
 		let  groupname = req.body.groupname;
-
 		firebase.auth().onAuthStateChanged((user) => {
 			if(user){
 				const groupKey =firebase.database().ref('group').push({
 					groupname : groupname
 				}).key;
-				res.send('You create Group name '+groupname);
+				res.send('You create Group name  '+groupname);
 			}
 			else {
 				res.send('User is not signed in');
@@ -20,6 +21,6 @@ module.exports = function (app) {
 	});
 
 	app.get('/group', function(req, res) {
-		res.send('want to create page? Use post method');
+		res.send('GROUP ROUTE!');
 	});
 };
