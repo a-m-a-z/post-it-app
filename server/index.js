1fd2bd5037ff964e2d2cb96f8ae34e0010cb225d
Ahmad Abdul-Aziz <ahmad.abdulaziz37@gmail.com>
2017-05-29 16:37:32 +0100

    add index file

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ module.exports =  (app) => {
 
 // SIGN-IN ROUTES ===================
 	app.get('/', function(req, res) {
-		res.send('WELCOME TO POSTIT ! ');
+		res.send('WELCOME TO POST-IT ! ');
+
 	});
 }
